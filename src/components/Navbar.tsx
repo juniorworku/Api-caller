64f23c59d26224b088dc22957e744ab26b829c5e
juniorworku <juniorworku@gmail.com>
2024-04-11 15:15:57 +0300

    Initializing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Component } from 'solid-js';
 const Navbar: Component = () => {
   return (
     <header class="bg-purple-400 text-white py-2 px-8 h-16 flex items-center justify-between">
-      <a class="hover:opacity-50 hero" href='/'>REST in Peace</a>
+      <a class="hover:opacity-50 hero" href='/'>API CALLER</a>
       <div class="flex items-center gap-4">
         <a class="hover:opacity-50" href='/about'>About</a>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,77 @@
-import { Component } from 'solid-js';
+import { Component, createSignal, For } from 'solid-js';
+import { A } from '@solidjs/router'; 
+import { restRequests, setRestRequests } from '../store/store';
+import RequestModal from '../components/RequestModal';
+import IconButton from '../components/IconButton';
+import "./Home.css"
+
 
 const Home: Component = () => {
-  return (
-    <div class="flex flex-col md:flex-row gap-4 h-full flex-1">
-      <div class="w-full md:w-1/4 bg-gray-200 min-h-full border-gray-300 border p-4 rounded-lg">
-        <div class="flex justify-between py-4">
-          <h1 class="text-sm ">Rest Requests</h1>
-          <button class="flex hover:bg-opacity-60 justify-center items-center p-4 bg-purple-600 rounded-full text-white w-8 h-8" onClick={() => alert('To be implemented')}>
-            <div>+</div>
-          </button>
+    const [showModal, setShowModal] = createSignal(false)
+    
+    return (
+        <div class="flex flex-col md:flex-row gap-4 h-full flex-1">
+            <div>
+                <RequestModal
+                    show={showModal()}
+                    onModalHide={(id: string | null) => {
+                    setShowModal(!showModal());
+                    }}
+                />
+            </div>
+            
+        <div class="w-full md:w-1/4 bg-gray-200 min-h-full border-gray-300 border p-4 rounded-lg">
+            <div class="flex justify-between py-4">
+            <h1 class="text-sm ">Rest Requests</h1>
+            <IconButton
+                onClick={() => setShowModal(true)}
+                icon="add"
+                label="Add Request"
+            />
+            </div>
+            <div class="list">
+                <For each={restRequests()} fallback={<div>Loading...</div>}>
+                {(item) => (
+                    <A href={`/${item.id}`} class="relative list__item">
+                    <div
+                        class="p-2 hover:bg-gray-300 cursor-pointer pr-12 rounded-lg mb-2"
+                        classList={{
+                        "list__item--active": Boolean(
+                            location.pathname === `/${item.id}`
+                        ),
+                        }}
+                    >
+                        <div>{item.name}</div>
+                        <div class="text-xs break-all">
+                        {item.request.method} {item.request.url}
+                        </div>
+                    </div>
+                    <ion-icon
+                        onclick={(e: MouseEvent) => {
+                            e.preventDefault();
+                            e.stopImmediatePropagation();
+                            if (restRequests()?.length) {
+                            const requests = restRequests() || [];
+                            setRestRequests(
+                                requests.filter((i) => i.id !== item.id)
+                            );
+                            //if (location.pathname === `/${item.id}`) {
+                             //   navigate("/");
+                            //}
+                            }
+                        }}
+                        class="absolute text-xl hover:scale-125 transition-all ease-in-out duration-100 hover:text-red-700 text-red-600 right-2 top-0 bottom-0 m-auto"
+                        name="trash"
+                    ></ion-icon>
+                    </A>
+                )}
+                </For>
+            </div>
         </div>
-      </div>
-      <div class="flex-1 min-h-full">
-      </div>
-    </div>
-  )
+        <div class="flex-1 min-h-full">
+        </div>
+        </div>
+    )
 }
 
 export default Home;
